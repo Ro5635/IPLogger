4e35436136ab62d0fa55d598719c0cef100ef58e
Robert Curran <ro5635@gmail.com>
2017-04-27 00:30:21 +0100

    Reformatted and general cleaning to app.py
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 app = Flask(__name__)
 mysql = MySQL()
- 
+
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '5635'
@@ -15,34 +15,33 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
+
 @app.route("/")
 def main():
-    
     return render_template('index.html')
 
 
-@app.route('/update',methods=['POST'])
+@app.route('/update', methods=['POST'])
 def update():
-    # Load the recived JSON data
+    # Load the received JSON data
     content = json.loads(request.get_json())
 
     _idNumber = content['id_number']
     _ip = request.remote_addr
- 
+
     # validate the received values
     if _idNumber:
 
-    	cursor.execute("UPDATE Servers SET ipaddress = %s WHERE id = %s", ( _ip, _idNumber,))
+        cursor.execute("UPDATE Servers SET ipaddress = %s WHERE id = %s", (_ip, _idNumber,))
 
         cursor.execute("SELECT * FROM Servers")
         data = cursor.fetchone()
 
         conn.commit()
 
-
-        return json.dumps({'status':200})
+        return json.dumps({'status': 200})
     else:
-        return json.dumps({'status': 400, 'error':'Required fields were missing'})
+        return json.dumps({'status': 400, 'error': 'Required fields were missing'})
 
 
 if __name__ == "__main__":
